e3257f62e6b8a47a0a65a035ff59046f50bd38d4
Abdur Rab Khan <abdurrabkhan3263@gmail.com>
2026-06-08 19:06:25 +0530

    feat: implement Throttling functionality and integrate it into the ThrottlingPage component.

--- src/app/(dashboard)/javascript/throttling/page.tsx
+++ src/app/(dashboard)/javascript/throttling/page.tsx
@@ -1,12 +1,16 @@
 import CodePreview from "@/components/code-preview/CodePreview";
-import React from "react";
+import { getCodeFromPath } from "@/utils/utils";
 
 function ThrottlingPage() {
+  const throttlingCode = getCodeFromPath(
+    "./javascript/throttling/throttling.ts",
+  );
+
   return (
     <CodePreview
+      code={throttlingCode}
       title="Throttling"
       description="Throttling limits how often a function can run over time by enforcing a minimum interval between calls. It's useful for improving performance when handling frequent events like scroll or resize."
-      code=""
     >
       <></>
     </CodePreview>
